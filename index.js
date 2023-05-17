9f8b40e8e7a1abb6cddec57395a41c0911e88479
tamarShapira <atifal2022@gmail.com>
2023-05-17 13:14:47 +0300

    my commit message

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import { createRequire } from 'module';
+
+const app=express()
+const port=3000
+const require = createRequire(import.meta.url);
+const sdk = require('api')('@render-api/v1.0#5yi85d37lhqedd36');
+
+sdk.auth('rnd_XZsL5XpsLyCXQokKpFZe588ryb37');
+sdk.getServices()
+  .then(({ data }) => console.log(data))
+  .catch(err => console.error(err));
+
+app.get('/',(req,res)=>{res.send('hello world!')})
+
+app.listen(port,()=>{
+    console.log(`node.js working!`)
+})
