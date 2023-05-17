caeb7bbbf20204a99b9dbbdea83c46366dff1817
sari055 <sari0548567232@gmail.com>
2023-05-17 13:24:08 +0300

    first commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port=3000
 const require = createRequire(import.meta.url);
 const sdk = require('api')('@render-api/v1.0#5yi85d37lhqedd36');
 
-sdk.auth('rnd_XZsL5XpsLyCXQokKpFZe588ryb37');
+sdk.auth('rnd_3ZWSYqtbcW0POVXk4KSl7GPdVvli');
 sdk.getServices()
   .then(({ data }) => console.log(data))
   .catch(err => console.error(err));
